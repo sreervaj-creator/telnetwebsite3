e0e2a35031e6e58e9b98254a158564ef0b91a2e7
sreervaj-creator <sreervaj@gmail.com>
2026-05-24 12:38:44 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function () {
-const navbar = document.getElementById('navbar');
-
-if(window.scrollY > 40){
-navbar.classList.add('scrolled');
-}else{
-navbar.classList.remove('scrolled');
-}
-});
